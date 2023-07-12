3fe705e34bb5fadf1923b3d87b4f7d1abd41f18b
Bartlomiej Lorenowicz <barteklorenowicz258@gmail.com>
2023-07-12 19:41:54 +0200

    add Autowired annotation in DbService

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -4,6 +4,7 @@ import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DbService {
 
+    @Autowired
     private final TaskRepository taskRepository;
 
 
