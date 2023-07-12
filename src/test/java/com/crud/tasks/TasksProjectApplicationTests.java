30e9dee16c404d0c4a25625342abe280e7059ae7
Bartlomiej Lorenowicz <barteklorenowicz258@gmail.com>
2023-07-12 20:37:07 +0200

    solved uncheck

--- src/test/java/com/crud/tasks/TasksProjectApplicationTests.java
+++ src/test/java/com/crud/tasks/TasksProjectApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TasksProjectApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
